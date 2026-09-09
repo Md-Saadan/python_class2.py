e5e81648dc9c810cea8ad73f0b4e624ac52953e7
Md-Saadan <m.h.s31727saadan@gmail.com>
2026-09-09 22:41:23 +0530

    File is Added

---
+++ Day 14.py
@@ -0,0 +1,22 @@
+#  write a program to check largest of 3 numbers by using if else
+num1=(input("Enter a number 1"))
+num2=(input("Enter a number 2"))
+num3=(input("Enter a number 3"))
+
+if num1>num2>num3:
+    print("num1 is greter")
+elif  num2>num1>num3:
+    print("num2 is greter")
+else:
+    print("num3 is greater")
+
+#  check if a number is odd and divisible by 5
+num=(int(input("Enter a number from user")))
+if num%2!=0 and num/5:
+    print("The Number is odd and divisible by 5")
+else:
+    print("The number is not odd and divisible by 5")
+
+
+
+ 
