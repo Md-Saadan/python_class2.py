c6f168e2928c587085f3e1b32ba825733c52be69
Md-Saadan <m.h.s31727saadan@gmail.com>
2026-09-09 22:31:13 +0530

    New File

---
+++ Task2.py
@@ -0,0 +1,3 @@
+def calculate_electricity_bill(consumers_data):
+    bil_list=[]
+    for name,units in consumer 
---
+++ class 11 Dictionary.py
@@ -0,0 +1,37 @@
+employee={
+    "empid":548,
+    "name":"ali",
+    "DOB":[2006,7,30],
+    "Gender":"male",
+    "CGPA":"8.5",
+    "Academics":{"year":"2002","degree":["BCA,MCA"]}
+
+    }
+print(employee)
+new_employee=employee.copy()
+print(new_employee)
+# new_employee.clear()
+# print(new_employee)
+print(list(employee.values()))
+print(list(employee.keys()))
+
+employee.pop
+new_employee.pop
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
