7103acd3b7cc9058fec0348f1257a435b098bf57
Md-Saadan <m.h.s31727saadan@gmail.com>
2026-09-09 22:44:51 +0530

    File is Added

---
+++ Day 17.py
@@ -0,0 +1,23 @@
+# Task 1
+amount=(int(input("Enter a number from user")))
+if amount>25000:
+    Discount=0.1
+elif amount>20000:
+    Discount=0.008
+elif amount>15000:
+    Discount=0.05
+elif amount>10000:
+    Discount=0.03
+else :
+    Discount=0
+print(f"Amount:{amount}")
+print(f"Discount:{amount*Discount}")
+print(f"Total:{amount-(amount*Discount)}")
+
+# Task 2
+
+
+
+     
+
+
